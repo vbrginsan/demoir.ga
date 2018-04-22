216ca712a328bc03a78cb72bdd5f219a87a8f090
vbrginsan <37362827+vbrginsan@users.noreply.github.com>
2018-04-23 01:16:25 +0700

    Add files via upload

---
+++ tbcache.sql
@@ -0,0 +1,22 @@
+﻿# Host: localhost  (Version 5.5.5-10.1.30-MariaDB)
+# Date: 2018-04-22 21:42:17
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Structure for table "tbcache"
+#
+
+DROP TABLE IF EXISTS `tbcache`;
+CREATE TABLE `tbcache` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Query` varchar(100) NOT NULL,
+  `DocId` varchar(150) NOT NULL,
+  `Value` float NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+#
+# Data for table "tbcache"
+#
+
