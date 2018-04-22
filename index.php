087f2ea7075c4ad164da81c2a7fc34cd2d490e20
vbrginsan <37362827+vbrginsan@users.noreply.github.com>
2018-04-23 01:14:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+  include "header.php"; 
+ ?>        
+                <h2>Selamat Datang</h2>
+                <p>Website ini di buat guna memenuhi Tugas UTS dari Mata Kuliah Information Retrieval, Program Studi Sistem Informasi, Fakultas Teknologi Informasi, Universitas Stikubank Semarang dan website ini masih dalam tahap pengembangan.</p>
+                <p>Mohon maaf bila masih ada banyak bug maupun error didalam aplikasi yang kami buat. Terima Kasih.</p>
+                <p>Salam</p>
+                <p></p>
+                <p>Admin</p>
+<?php 
+include "footer.php";
+ ?>
